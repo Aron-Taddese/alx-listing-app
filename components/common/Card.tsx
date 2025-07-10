d03c5176df301a0449e40277f7be73b51c7a6932
Aron-Alemu <arontaddesealemu@gmail.com>
2025-07-10 05:04:44 -0400

    fixing errors

---
+++ components/common/Card.tsx
@@ -0,0 +1,13 @@
+
+
+import { CardProps } from '../../interfaces/index';
+
+const Card = ({ title, description, imageUrl }: CardProps) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>{description}</p>
+      {imageUrl && <img src={imageUrl} alt={title} />}
+    </div>
+  );
+};
